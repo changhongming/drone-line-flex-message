d1726d1a874a0400e4ad8df5803dd59ba98355c7
Kevin <kevin.chang@kiwi-tec.com>
2020-09-11 10:35:46 +0800

    add lint and ci pipeline

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 (async function () {
-    const axios = require('axios');
-    const fs = require('fs').promises;
-    const path = require('path');
-    const Handlebars = require('handlebars');
+  const axios = require('axios');
+  const fs = require('fs').promises;
+  const path = require('path');
+  const Handlebars = require('handlebars');
 
-    // register template function
-    Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-    });
-    Handlebars.registerHelper('defaultVal', function (val, defaultVal) {
-        return new Handlebars.SafeString((val || defaultVal).replace(/\r?\n/, ""));
-    });
-    Handlebars.registerHelper('ifOr', function (arg1, arg2, options) {
-        if (arg1 || arg2) {
-            return options.fn(this);
+  // register template function
+  Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+  });
+  Handlebars.registerHelper('defaultVal', function (val, defaultVal) {
+    return new Handlebars.SafeString((val || defaultVal).replace(/\r?\n/, ''));
+  });
+  Handlebars.registerHelper('ifOr', function (arg1, arg2, options) {
+    if (arg1 || arg2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+
+  // dump line auth option
+  const { PLUGIN_TO, PLUGIN_CHANNEL_TOKEN } = process.env;
+  if (!(PLUGIN_CHANNEL_TOKEN || PLUGIN_TO)) {
+    console.error(`'CHANNEL_TOKEN' or 'TO' not provide!`);
+    process.exit(1);
+  }
+
+  try {
+    // try compile template json
+    const template = Handlebars.compile(await fs.readFile(
+      path.join(__dirname, 'template.json.temp'),
+      'utf-8'));
+    const payload = template(process.env);
+
+    // send line message
+    await axios.post('https://api.line.me/v2/bot/message/push',
+      {
+        'to': PLUGIN_TO,
+        'messages': JSON.parse(payload)
+      },
+      {
+        headers:
+        {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${PLUGIN_CHANNEL_TOKEN}`
         }
-        return options.inverse(this);
-    });
-
-    // dump line auth option
-    const { PLUGIN_TO, PLUGIN_CHANNEL_TOKEN } = process.env;
-    if (!(PLUGIN_CHANNEL_TOKEN || PLUGIN_TO)) {
-        console.error("'PLUGIN_CHANNEL_TOKEN' or 'TO' not provide!");
-        process.exit(1);
-    }
-
-    try {
-        // try compile template json
-        const template = Handlebars.compile(await fs.readFile(
-            path.join(__dirname, 'template.json.temp'),
-            'utf-8'));
-        const payload = template(process.env);
-
-        // send line message
-        await axios.post('https://api.line.me/v2/bot/message/push',
-            {
-                'to': PLUGIN_TO,
-                'messages': JSON.parse(payload)
-            },
-            {
-                headers:
-                {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${PLUGIN_CHANNEL_TOKEN}`
-                }
-            }
-        )
-        console.log('Send Finish');
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+      }
+    );
+    console.log('Send Finish');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
 })();
 
